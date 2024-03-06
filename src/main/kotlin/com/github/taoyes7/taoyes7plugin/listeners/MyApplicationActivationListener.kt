17faeb3c3b68f3a6cc8993d7772c47d344bedf48
GitHub Action <action@github.com>
2024-03-06 06:54:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/taoyes7/taoyes7plugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/taoyes7/taoyes7plugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.taoyes7.taoyes7plugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
